3fe6f653eda823ef83ed99b130dbff050eb8a1c0
lxx <853555374@qq.com>
2019-01-02 19:04:32 +0800

    use Interceptor and token toAdduser

---
+++ myopencart-parent/opencart-admin-webapi/src/main/java/io/lxx/opencartadminwebapi/admin/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package io.lxx.opencartadminwebapi.admin.config;
+
+import io.lxx.opencartadminwebapi.admin.interceptor.TokenInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Autowired
+    private TokenInterceptor tokenInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(tokenInterceptor);
+    }
+}
---
+++ myopencart-parent/opencart-admin-webapi/src/main/java/io/lxx/opencartadminwebapi/admin/interceptor/TokenInterceptor.java
@@ -0,0 +1,59 @@
+package io.lxx.opencartadminwebapi.admin.interceptor;
+
+import com.alibaba.fastjson.JSON;
+import io.lxx.opencartadminwebapi.admin.DTO.LoginInfo;
+import io.lxx.opencartadminwebapi.admin.exception.BackendClientException;
+import io.lxx.opencartadminwebapi.admin.exception.BackendUnauthenticationException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class TokenInterceptor implements HandlerInterceptor {
+
+    private String[] urls = {"/user/login","/error"};
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws BackendUnauthenticationException, BackendClientException {
+        boolean contains = Arrays.asList(urls).contains(request.getRequestURI());//是否包括uri
+        if(contains){
+            return true;
+        }
+        String authorizationStr = request.getHeader("Authorization");//获取头信息
+        if(authorizationStr == null){
+            throw new BackendUnauthenticationException("Unauthentication");
+        }
+        String[] s = authorizationStr.split(" ");//解析头信息
+        String token = s[1];
+        LoginInfo loginInfo;
+        try {
+            byte[] decode = Base64.getDecoder().decode(token);//base64解码
+            String loginJsonStr = new String(decode, "UTF-8");
+            loginInfo = JSON.parseObject(loginJsonStr,LoginInfo.class);
+        } catch (Exception e) {
+            throw new BackendClientException("auth invalid caused by some issues");
+        }
+        Long userId = loginInfo.getUserId();//获取用户id
+        String username = loginInfo.getUsername();
+        if(username == null){
+            throw new BackendUnauthenticationException("Unauthentication");
+        }
+        long expireTimestamp = loginInfo.getExpirationTime().getTime();//获取过期时间戳
+        Date currentTime = new Date();
+        long currentTimestamp = currentTime.getTime();//当前时间戳
+        if(username == null || username.isEmpty()){
+            throw new BackendUnauthenticationException("Unauthentication: username is null or empty");
+        }
+        if(currentTimestamp > expireTimestamp){
+            throw new BackendUnauthenticationException("Unauthentication: token is expired");
+        }
+        request.setAttribute("userId",userId);
+        request.setAttribute("username",username);
+        return true;
+    }
+}
