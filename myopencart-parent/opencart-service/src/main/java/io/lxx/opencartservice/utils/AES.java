b2446318e29aefcb8ed7a534aaf63d7a3ac6187f
lxx <853555374@qq.com>
2019-01-22 10:59:21 +0800

    add base api down

--- myopencart-parent/opencart-service/src/main/java/io/lxx/opencartservice/utils/AES.java
+++ myopencart-parent/opencart-service/src/main/java/io/lxx/opencartservice/utils/AES.java
@@ -11,27 +11,26 @@ public class AES {
     private static SecretKeySpec secretKey;
     private static byte[] key;
 
-    //自定义构造方法 设置秘钥
-    public static void setKey(String mykey) throws Exception {
+    public static void setKey(String myKey) throws Exception {
         MessageDigest sha = null;
-        key = mykey.getBytes("UTF-8");
+        key = myKey.getBytes("UTF-8");
         sha = MessageDigest.getInstance("SHA-1");
         key = sha.digest(key);
-        key = Arrays.copyOf(key,16);
+        key = Arrays.copyOf(key, 16);
         secretKey = new SecretKeySpec(key, "AES");
     }
-    //加密方法
-    public static String encrypt(String strToEncrypt,String secret) throws Exception {
+
+    public static String encrypt(String strToEncrypt, String secret) throws Exception {
         setKey(secret);
-        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");//AES/ECB/PKCS5Padding加密算法
-        cipher.init(Cipher.ENCRYPT_MODE,secretKey);
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
         return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
     }
-    //解密方法
-    public static String decrypt(String strToDecrypt,String secret) throws Exception {
+
+    public static String decrypt(String strToDecrypt, String secret) throws Exception {
         setKey(secret);
-        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");//AES/ECB/PKCS5Padding加密算法
-        cipher.init(Cipher.ENCRYPT_MODE,secretKey);
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
         return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
     }
 }
